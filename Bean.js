9fa5706b29633f92a648ec00fcab62ebc51f9ea5
Y-Navyadevi <navyadevi789@msitprogram.net>
2021-03-15 16:04:39 +0530

    Function 2

--- Bean.js
+++ Bean.js
@@ -1,10 +1,25 @@
-function countBs(s) {
+function countBs(str) {
     var count = 0; 
-    for (var i = 0; i < s.length; i += 1) {
-      if (s.charAt(i) === "B")
-        count += 1;
+    for (var i = 0; i < str.length; i++) {
+      if (str.charAt(i) === "B")
+        count++;
     }
     return count;
   }
-  console.log(countBs("BBCbbbbBB"));
-  
+  console.log(countBs("Hello B the count of b is equals to B so BBBB"));
+
+
+
+
+  var countChar = function(str, character) {
+    var count = 0;
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] === character)
+            count++;
+    }
+    return count;
+}
+console.log(countChar('Submission guidelines', 'i'))
+console.log(countChar('Submission guidelines', 's'));
+
+
