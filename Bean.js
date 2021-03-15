d350394aaf931bce13fabb7521449e955492a441
Y-Navyadevi <navyadevi789@msitprogram.net>
2021-03-15 15:36:57 +0530

    Function 1

--- Bean.js
+++ Bean.js
@@ -1,1 +1,10 @@
-console.log('Hello World');
+function countBs(s) {
+    var count = 0; 
+    for (var i = 0; i < s.length; i += 1) {
+      if (s.charAt(i) === "B")
+        count += 1;
+    }
+    return count;
+  }
+  console.log(countBs("BBCbbbbBB"));
+  
